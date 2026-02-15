12b8e6b1cc1aacec10ddd1c338d17b27c01d7bfc
Nyla Prince <nyla.prince@georgebrown.ca>
2026-02-14 22:27:43 -0500

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,28 @@
+# Sample coding Questions 01 Week 01
+# Nyla Prince 
+
+# Question 2: Defining an array
+num_array = [1, 4, 7, 9]
+
+#Question 3:
+
+a = 1
+b = 2
+c = 3
+d = 4
+
+e = (a - ((b ** c) // d) + (a % c))
+
+#Question 4: 
+
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius")
+
+
+#Question 5:    
+userAge = int(input("Enter your age: "))   
+
+userAge = userAge + 22   
+print("Now showing the shop items filtered by age: " ,userAge)
+
+
